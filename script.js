1b2cbd6c4e11025110517e1bf00ee90dbcd9ff55
Asta Remeikiene <65591369+AstaRem@users.noreply.github.com>
2023-04-02 18:42:44 +0100

    broken

--- script.js
+++ script.js
@@ -1,48 +1,63 @@
 let currentTime = $("#currentDay");
 let clickBtn = $('.clickBtn');
-let clickBtn2 = $('.clickBtn');
+// let clickBtn2 = $('.clickBtn');
 let task1 = $('#task1');
 let task2 = $('#task2');
+let tasks = $('.task');
+let taskId = $('div.row[id]');
 
 
-let timeNow = moment().format('MMMM Do YYYY, h:mm a')
+//select all buttons
+let buttons = $('.clickBtn');
+console.log(tasks.length)
+
 $(function(){
-    currentTime.html(timeNow);
-
-    clickBtn.on('click',  function(){
-        console.log('Save button was clicked.')
-        //now save info from input text field to local storage
-        let userInput = task1.val();
-        localStorage.setItem("task1", userInput);
-
-        // let userInput2 = task2.val();
-        // localStorage.setItem("task2", userInput2);
-
-    })
-    clickBtn2.on('click',  function(){
-        console.log('Save button 2 was clicked.')
-        //now save info from input text field to local storage
-        let userInput2 = task2.val();
-        localStorage.setItem("task2", userInput2);
-
+    $("#hour-9, button").click(function(){
+    
     })
 
-    savedTask1 = localStorage.getItem("task1");
-    console.log(savedTask1);
-    task1.val(savedTask1);
+    })
+//show date in description
+// let timeNow = moment().format('MMMM Do YYYY, h:mm a')
+// $(function(){
+//     currentTime.html(timeNow);
 
-    savedTask2 = localStorage.getItem("task2");
-    console.log(savedTask2);
-    task2.val(savedTask2);
+//     clickBtn.on('click',  function(){
+//         console.log('Save button was clicked.')
+//         //now save info from input text field to local storage
+//         let userInput = task1.val();
+//         localStorage.setItem("task1", userInput);
+
+//         // let userInput2 = task2.val();
+//         // localStorage.setItem("task2", userInput2);
+
+//     })
+//     clickBtn2.on('click',  function(){
+//         console.log('Save button 2 was clicked.')
+//         //now save info from input text field to local storage
+//         let userInput2 = task2.val();
+//         localStorage.setItem("task2", userInput2);
+
+//     })
+
+//     savedTask1 = localStorage.getItem("task1");
+//     console.log(savedTask1);
+//     task1.val(savedTask1);
+
+//     savedTask2 = localStorage.getItem("task2");
+//     console.log(savedTask2);
+//     task2.val(savedTask2);
+
+
+// //now - how to identify each task?
+// //will need time orientation - to mark now-past-future with colors
+// //instead of clickBtn2.on(), clickBtn3.on(), write loop for each? select all of them with jquery - it will return an array, then say each()
 
 
 
+// })
 
-
-
-})
-
-//get a single line time today
-//converts time to 24-hour format
-let convertedTime = moment("01:00 PM", 'hh:mm A').format('HH:mm')
-console.log(convertedTime);
+// //get a single line time today
+// //converts time to 24-hour format
+// let convertedTime = moment("01:00 PM", 'hh:mm A').format('HH:mm')
+// console.log(convertedTime);
